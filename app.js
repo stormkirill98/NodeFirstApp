10111e72b7fa1e8d6f33c856ecb4b4c810240b96
kPanihin <k.panihin@akolchin.com>
2019-12-04 13:04:13 +0300

    setup nodemon

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
+app.get('/save', (req, res) => {
+    if (req.method === 'POST') {
+
+    } else {
+        res.status(501);
+        res.end("501 This method is not implemented");
+    }
 });
 
 app.listen(8080);
