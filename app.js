8b337eab467232dd5386afb6ed2b9fdb4806eca1
kPanihin <k.panihin@akolchin.com>
2019-12-04 12:33:43 +0300

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("Hello World");
+    res.end();
+}).listen(8080);
+
+console.log("Server Running on 8080");
