e5c5401fa8a608db8a9ca1c2f8ca8bce78965dbc
kPanihin <k.panihin@akolchin.com>
2019-12-04 13:19:14 +0300

    parse and output to console body

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.get('/save', (req, res) => {
+app.all('/save', (req, res) => {
+    console.log("handle request");
     if (req.method === 'POST') {
-
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            console.log(
+                body
+            );
+            res.end('ok');
+        });
     } else {
         res.status(501);
         res.end("501 This method is not implemented");
